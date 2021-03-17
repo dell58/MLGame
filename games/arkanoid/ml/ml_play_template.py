d08627313a43cda243e682f6082d5c52d02c719f
dell58 <dell58@kimo.com>
2021-03-17 18:06:15 +0800

    Add upper vector check

--- games/arkanoid/ml/ml_play_template.py
+++ games/arkanoid/ml/ml_play_template.py
@@ -9,8 +9,8 @@ class MLPlay:
         Constructor
         """
         self.ball_served = False
-        self.ball_prev = None
         self.vector_prev = (0,0)
+        self.tmp = 0
 
     def update(self, scene_info):
         """
@@ -19,18 +19,19 @@ class MLPlay:
         # Make the caller to invoke `reset()` for the next round.
         if (scene_info["status"] == "GAME_OVER" or
             scene_info["status"] == "GAME_PASS"):
+            print(scene_info['ball'])
+            print(scene_info['platform'])
+            print(self.tmp)
+            print(self.vector_prev)
             return "RESET"
 
         if not self.ball_served:
             self.ball_served = True
             self.ball_prev = scene_info['ball']
-            command = 'SERVE_TO_RIGHT'
+            command = 'SERVE_TO_LEFT'
         else:
             ball_cur = scene_info['ball']
-            platform_pos = scene_info['platform']
-            bricks = scene_info['bricks']
-            h_bricks = scene_info['hard_bricks']
-            p_x = platform_pos[0] + 20 # platform middle
+            p_x = scene_info['platform'][0] + 20
             vector = (ball_cur[0] - self.ball_prev[0],ball_cur[1] - self.ball_prev[1])
             if (vector[1] > 0): # ball fall
                 offset = (vector[0] / vector[1]) * (400 - ball_cur[1])
@@ -38,21 +39,31 @@ class MLPlay:
                 if(ballx_update + 5 > 200):
                     ballx_update =  200 - (ballx_update + 5 - 200)
                 elif (ballx_update < 0):
-                    ballx_update *=-1
+                    ballx_update *= -1
                 pass
-                if (ballx_update < platform_pos[0]):
+                self.vector_prev = vector
+                self.tmp = ballx_update
+                if (ballx_update < p_x and abs(p_x-ballx_update) > 15):
                     command = 'MOVE_LEFT'
-                elif (ballx_update > platform_pos[0] + 40):
+                elif (ballx_update > p_x and abs(p_x-ballx_update) > 15):
                     command = 'MOVE_RIGHT'
                 else:
                     command = 'NONE'
-            else: # move platform to center
-                if( p_x > 100):
-                    command = 'MOVE_LEFT'
-                elif ( p_x < 100):
-                    command  = 'MOVE_RIGHT'
+            else: # ball climbing
+                if(ball_cur[1] >=300): # move platform to the same direction of ball
+                    if( vector[0] > 0 and p_x < 155):
+                        command = 'MOVE_RIGHT'
+                    elif ( vector[0] < 0 and p_x > 45):
+                        command  = 'MOVE_LEFT'
+                    else:
+                        command = 'NONE'
                 else:
-                    command ='NONE'
+                    if(p_x > 100):
+                        command = 'MOVE_LEFT'
+                    elif(p_x < 100):
+                        command = 'MOVE_RIGHT'
+                    else:
+                        command = 'NONE'
             self.ball_prev = ball_cur
         return command
 
@@ -61,5 +72,4 @@ class MLPlay:
         Reset the status
         """
         self.ball_served = False
-        self.ball_prev = None
         self.vector_prev = (0,0)
